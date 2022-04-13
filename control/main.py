ebc3022f29ed2a9e5feed79babb9c2a02b183fb0
rdh1115 <mark1026bai@gmail.com>
2022-04-12 20:04:13 -0400

    icm module, render

--- control/main.py
+++ control/main.py
@@ -207,6 +207,7 @@ def main():
                     last_state,
                     state,
                     action, device)
+                bonus = torch.tensor(1, device=device)
                 bonus = for_loss.detach() * args.eta
                 reward = reward + bonus.to(device)  # .clamp(-1.0, 1.0)
             # If done then clean the history of observations.
