3c14595a96426a93510bb88aba43a23978557208
rdh1115 <mark1026bai@gmail.com>
2022-04-12 20:20:01 -0400

    icm module, render

--- control/main.py
+++ control/main.py
@@ -205,7 +205,7 @@ def main():
                     last_state,
                     state,
                     action, device)
-                bonus = for_loss * args.eta
+                bonus = for_loss.detach() * args.eta
                 reward = reward + bonus.cpu()
             # If done then clean the history of observations.
             masks = torch.FloatTensor([[0.0] if done_ else [1.0] for done_ in done])
